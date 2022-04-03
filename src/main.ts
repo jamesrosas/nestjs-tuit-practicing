c746f15b14423f2b8b265a84bc3fba93cd1167e0
James Rosas <jamesadrian777@gmail.com>
2022-04-02 20:16:54 -0500

    connecting with postgres db (docker-compose.yml)

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({
-    transform: true
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true
   }))
+  // es buena practica hacer el uso de whitelist para que nos filtre solo la informacion que se solciita, forbidenNonWhitelisted es opcional para porder lanazar un 400 bad request.
+
   await app.listen(3000);
 }
 bootstrap();
